f0c49dffc68cc38ed97b9bea4e202653b4b67f89
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-01-01 18:06:04 +0530

    signup route completed

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // allows to parse the body of request
+
 //import routes 
 import authRoutes from "./routes/auth.route.js"
-
+ 
 
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
+  connectDB();
+
 });
  
