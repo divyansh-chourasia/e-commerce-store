2c9b9fa9b3db06e9540e7c3afa206629460a288a
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-01-01 20:33:25 +0530

    auth controller completed (signup, login, logout)

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config(); 
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows to parse the body of request
+app.use(cookieParser());
 
 //import routes 
 import authRoutes from "./routes/auth.route.js"
