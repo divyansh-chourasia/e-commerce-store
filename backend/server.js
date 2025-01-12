f501efd626d5ac4fed8347bef56117a825383c62
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-01-12 17:53:34 +0530

    analytics route completed

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 
  
 
@@ -25,6 +26,8 @@ app.use("/api/products", productRoutes);
 app.use("/api/products", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
